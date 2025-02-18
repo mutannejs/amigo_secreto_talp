a2e19830c08efaf7aec0c3cbc04e2e2df73924cc
mutannejs <murillojustinoklein@gmail.com>
2025-02-17 22:50:36 -0300

    iniciado visualização dos eventos do usuário

--- lib/pages/events/events_page.dart
+++ lib/pages/events/events_page.dart
@@ -1,7 +1,10 @@
 import 'package:amigo_secreto_talp/utils/localization/locales.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:go_router/go_router.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 const eventsPT = {
   "eventsTitle": "Meus eventos",
@@ -14,6 +17,9 @@ const eventsEN = {
   "eventsSubtitle": 'Click on the event to see who you drew.',
   "eventsButton": "See details",
 };
+
+final FirebaseFirestore db = FirebaseFirestore.instance;
+final FirebaseAuth auth = FirebaseAuth.instance;
 
 class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
@@ -30,8 +36,16 @@ class _EventsPageState extends State<EventsPage> {
     'lululu',
   ];
 
+  Future<QuerySnapshot<Map<String, dynamic>>> getEvents () {
+    return db
+      .collection("eventos")
+      .get();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
     return Padding(
       padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
       child: Column(
@@ -46,37 +60,52 @@ class _EventsPageState extends State<EventsPage> {
             style: Theme.of(context).textTheme.bodyMedium,
           ),
           const SizedBox(height: 20),
-          Flexible( // Lista de eventos
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return ListTile(
-                  title: Text(item),
-                  subtitle: Text(item),
-                  shape: index != items.length-1 ? Border(
-                    bottom: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 1,
-                    )
-                  ) : null,
-                  trailing: OutlinedButton.icon(
-                    onPressed: () => context.go(
-                      Uri(
-                        path: '/event',
-                        queryParameters: {
-                          'event': 'meueventoid',
-                          'user': 'meuuserid'
-                        }
-                      ).toString()
-                    ),
-                    icon: const Icon( Icons.remove_red_eye_outlined ),
-                    label: Text(AppLocale.eventsButton.getString(context)),
+          FutureBuilder(
+            future: getEvents(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Flexible( // Lista de eventos
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      return ListTile(
+                        title: Text(item),
+                        subtitle: Text(item),
+                        shape: index != items.length-1 ? Border(
+                          bottom: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 1,
+                          )
+                        ) : null,
+                        trailing: OutlinedButton.icon(
+                          onPressed: () => context.go(
+                            Uri(
+                              path: '/event',
+                              queryParameters: {
+                                'event': 'meueventoid',
+                                'user': 'meuuserid'
+                              }
+                            ).toString()
+                          ),
+                          icon: const Icon( Icons.remove_red_eye_outlined ),
+                          label: Text(AppLocale.eventsButton.getString(context)),
+                        ),
+                        titleTextStyle: Theme.of(context).textTheme.displaySmall,
+                      );
+                    }
                   ),
-                  titleTextStyle: Theme.of(context).textTheme.displaySmall,
+                );
+              } else {
+                return const SingleChildScrollView(
+                  child: IntrinsicWidth(
+                    child: LoadingIndicator(
+                      indicatorType: Indicator.ballRotate,
+                    ),
+                  ),
                 );
               }
-            ),
+            },
           ),
         ]
       )
