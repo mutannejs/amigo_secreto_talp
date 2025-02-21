4f1b1e17220c84dfee98561de33789b1d27c7042
jbrants <joaobrants@estudante.ufscar.br>
2025-02-21 18:40:54 -0300

    nome evento e pi

--- lib/pages/form/form_page.dart
+++ lib/pages/form/form_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 final FirebaseFirestore db = FirebaseFirestore.instance;
 final FirebaseAuth auth = FirebaseAuth.instance;
@@ -15,9 +16,11 @@ class FormPage extends StatefulWidget {
 
 class _FormPageState extends State<FormPage> {
   final List<Map<String, TextEditingController>> _controllers = [];
+  final TextEditingController _eventController = TextEditingController(); // Adicione este controlador
   int _numberOfPeople = 1;
   User? user = auth.currentUser;
-  
+  Future<void>? _submitFuture;
+
   void _addPersonFields() {
     setState(() {
       _controllers.add({
@@ -37,12 +40,25 @@ class _FormPageState extends State<FormPage> {
     _controllers.shuffle();
   }
 
+  Future<void> _submitForm() async {
+    final data = _controllers.map((controller) => {
+          'name': controller['name']!.text,
+          'email': controller['email']!.text,
+        }).toList();
+    await db.collection("eventos").add({
+      'data': data,
+      'user': user!.uid,
+      'evento': _eventController.text, // Inclua o valor do campo de evento
+    });
+  }
+
   @override
   void dispose() {
     for (var controller in _controllers) {
       controller['name']?.dispose();
       controller['email']?.dispose();
     }
+    _eventController.dispose(); // Dispose do controlador do campo de evento
     super.dispose();
   }
 
@@ -56,6 +72,13 @@ class _FormPageState extends State<FormPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            TextField(
+              controller: _eventController,
+              decoration: const InputDecoration(
+                labelText: 'Evento',
+              ),
+            ),
+            SizedBox(height: 16.0),
             Row(
               children: [
                 const Text('Número de pessoas:'),
@@ -108,29 +131,50 @@ class _FormPageState extends State<FormPage> {
                 },
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                _shufflePeople();
-
-                for (var controller in _controllers) {
-                  print('Nome: ${controller['name']!.text}');
-                  print('Email: ${controller['email']!.text}');
-                }
-                final data = _controllers.map((controller) => {
-                      'name': controller['name']!.text,
-                      'email': controller['email']!.text,
-                    }).toList();
-                db.collection("eventos").add({'data': data, 'user': user!.uid});
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: const Text('Formulário enviado com sucesso!'),
+            _submitFuture == null
+                ? ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _shufflePeople();
+                        _submitFuture = _submitForm().then((_) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text('Formulário enviado com sucesso!'),
+                            ),
+                          );
+                          context.go('/home');
+                        });
+                      });
+                    },
+                    child: const Text('Enviar'),
+                  )
+                : FutureBuilder<void>(
+                    future: _submitFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return SpinKitWave(
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 50.0,
+                        );
+                      } else if (snapshot.hasError) {
+                        return Column(
+                          children: [
+                            Text('Erro: ${snapshot.error}'),
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _submitFuture = null;
+                                });
+                              },
+                              child: const Text('Tentar novamente'),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Container(); // Retorna um container vazio após o sucesso
+                      }
+                    },
                   ),
-                );
-
-                context.go('/home');
-              },
-              child: const Text('Enviar'),
-            ),
           ],
         ),
       ),
